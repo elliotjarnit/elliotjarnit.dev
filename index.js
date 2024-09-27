50112228943935a158d99c1036329ad868a64ffc
elliotjarnit <me@elliotjarnit.dev>
2024-09-27 11:16:06 -0700

    Change some styling with the icon circles

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const icons = ["bun", "cloudflare", "cplusplus", "css3", "electron", "express", "figma", "firebase", "framermotion", "git", "github", "go", "googlecloud", "gradle", "graphql", "html5", "java", "javascript", "kotlin", "lua", "mongodb", "mysql", "nextjs", "nodejs", "nuxtjs", "objectivec", "openapi", "opengl", "opencl", "php", "postgresql", "prisma", "python", "react", "redis", "rust", "sass", "socketio", "sqlite", "supabase", "svelte", "swift", "tailwindcss", "tauri", "tensorflow", "threejs", "typescript", "vuejs"]
 
+const innerCircleItemCount = 10
+const outerCircleItemCount = 16
+
 function pickIcons(count) {
   const pickedIcons = []
   const availableIcons = [...icons]
@@ -17,14 +20,14 @@ function populateIcons() {
   const circle1 = document.getElementById("circle-1")
   const circle2 = document.getElementById("circle-2")
 
-  const icons = pickIcons(32)
+  const icons = pickIcons(innerCircleItemCount + outerCircleItemCount)
 
   circle1.innerHTML = ""
   circle2.innerHTML = ""
 
   icons.forEach((icon, index) => {
-    const circle = index < 16 ? circle1 : circle2
-    const itemCount = 16
+    const circle = index < innerCircleItemCount ? circle1 : circle2
+    const itemCount = index < innerCircleItemCount ? innerCircleItemCount : outerCircleItemCount
     circle.innerHTML += `<div class="dev-icon" style="--rot: ${index * (360 / itemCount)}"><img src="icons/${icon}.svg" alt="${icon}" /></div>`
   })
 }
